fad67127097aad8e6d42ff9b74e8c8ac55cdccf1
Tan Tien <tiennhtse173499@fpt.edu.vn>
2024-07-16 19:18:21 +0700

    Add send email for Trade order and Exchange Order

--- src/ExchangeGood.Service/UseCase/OrderService.cs
+++ src/ExchangeGood.Service/UseCase/OrderService.cs
@@ -16,6 +16,10 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using ExchangeGood.Repository;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using ExchangeGood.DAO;
 
 namespace ExchangeGood.Service.UseCase
 {
@@ -25,27 +29,34 @@ namespace ExchangeGood.Service.UseCase
         private readonly IProductService _productService;
         private readonly IMemberService _memberService;
         private readonly IUnitOfWork _uow;
+        private readonly SmtpSettings _smtpSetting;
 
-        public OrderService(IOrderRepository orderRepository, IProductService productService, IMemberService memberService, IUnitOfWork uow)
+        public OrderService(IOrderRepository orderRepository, IProductService productService, IMemberService memberService, IUnitOfWork uow, IOptions<SmtpSettings> smtpSetting)
         {
             _orderRepository = orderRepository;
             _productService = productService;
             _memberService = memberService;
             _uow = uow;
+            _smtpSetting = smtpSetting.Value;
+
         }
 
-        public async Task<bool> CreateOrdersForExchange(CreateOrderExchangeRequest createOrderRequest) {
+        public async Task<bool> CreateOrdersForExchange(CreateOrderExchangeRequest createOrderRequest)
+        {
             var notification = await _memberService.GetNotificationsById(createOrderRequest.NotificationID);
-            if(notification == null) {
+            if (notification == null)
+            {
                 throw new BadRequestException("ExchangeRequest does not exist.");
             }
 
-            if(notification.RecipientId != createOrderRequest.OwnerID) {
+            if (notification.RecipientId != createOrderRequest.OwnerID)
+            {
                 throw new Exception("Wrong User.");
             }
             // checkout order type Exchange
             using var transaction = _uow.BeginTransaction();
-            try {
+            try
+            {
                 // create Order for owner who has a product for other want to exchange
                 var product = await _productService.GetProduct(int.Parse(notification.OnwerProductId));
                 if (product.Status == Contract.Enum.Product.Status.Sold.Name) throw new BadRequestException($"{product.Title} was sold out");
@@ -54,7 +65,8 @@ namespace ExchangeGood.Service.UseCase
                     throw new BadRequestException($"Product {product.ProductId} is not belong to user {createOrderRequest.OwnerID}");
                 }*/
                 // Create order for the sender -> it like the sender buy the recipient's product
-                var orderForSender = new Order() {
+                var orderForSender = new Order()
+                {
                     BuyerId = notification.SenderId,
                     CreatedTime = DateTime.UtcNow,
                     UpdatedTime = DateTime.UtcNow,
@@ -71,19 +83,22 @@ namespace ExchangeGood.Service.UseCase
                 await _orderRepository.AddOrder(orderForSender);
 
                 List<OrderDetail> orderDetailsOfOwner = new();
-                foreach (var productExchangeId in notification.ExchangerProductIds.Split(',')) {
+                foreach (var productExchangeId in notification.ExchangerProductIds.Split(','))
+                {
                     var exchangeProduct = await _productService.GetProduct(int.Parse(productExchangeId));
                     if (exchangeProduct.Status == Contract.Enum.Product.Status.Sold.Name) throw new BadRequestException($"{product.Title} was sold out");
                     exchangeProduct.Status = Contract.Enum.Product.Status.Sold.Name;
 
-                    orderDetailsOfOwner.Add(new OrderDetail() {
+                    orderDetailsOfOwner.Add(new OrderDetail()
+                    {
                         ProductId = int.Parse(productExchangeId),
                         SellerId = notification.SenderId,
                         Amount = 0,
                     });
                 }
                 // create Order for recipiten who own the product and submit this request-> like that user buy the products of sender
-                var orderForOwner = new Order() {
+                var orderForOwner = new Order()
+                {
                     BuyerId = createOrderRequest.OwnerID,
                     CreatedTime = DateTime.UtcNow,
                     UpdatedTime = DateTime.UtcNow,
@@ -96,6 +111,9 @@ namespace ExchangeGood.Service.UseCase
                 await _orderRepository.AddOrder(orderForOwner);
 
                 transaction.Commit();
+
+                await SendOrderExchangeRequestEmail(createOrderRequest.OwnerID);
+
                 return true;
             }
             catch (Exception)
@@ -108,35 +126,35 @@ namespace ExchangeGood.Service.UseCase
         public async Task<bool> UpdateOrderStatus(int orderId, bool isCompleted = true)
         {
             using var transaction = _uow.BeginTransaction();
-                try
-                {
-                    // get order
-                    Order order = await _orderRepository.GetOrder(orderId) ?? throw new OrderNotFoundException(orderId);
-                    // check order is completed 
-                    order.Status = isCompleted
-                        ? Contract.Enum.Order.Status.Completed.Name
-                        : Contract.Enum.Order.Status.Cancelled.Name;
-                    
-                    // check if order status is cancelled => set product status back to selling
-                    if (order.Status.Equals(Contract.Enum.Order.Status.Cancelled.Name))
-                    {
-                        foreach (var orderDetail in order.OrderDetails)
-                        {
-                            var product = await _productService.GetProduct(orderDetail.ProductId) ??
-                                          throw new ProductNotFoundException(orderDetail.ProductId);
-                            product.Status = Contract.Enum.Product.Status.Selling.Name;
-                        }
-                    }
-                    await _orderRepository.UpdateOrder(order);
+            try
+            {
+                // get order
+                Order order = await _orderRepository.GetOrder(orderId) ?? throw new OrderNotFoundException(orderId);
+                // check order is completed 
+                order.Status = isCompleted
+                    ? Contract.Enum.Order.Status.Completed.Name
+                    : Contract.Enum.Order.Status.Cancelled.Name;
 
-                    transaction.Commit();
-                    return true;
-                }
-                catch (Exception e)
+                // check if order status is cancelled => set product status back to selling
+                if (order.Status.Equals(Contract.Enum.Order.Status.Cancelled.Name))
                 {
-                    transaction.Rollback();
-                    return false;
+                    foreach (var orderDetail in order.OrderDetails)
+                    {
+                        var product = await _productService.GetProduct(orderDetail.ProductId) ??
+                                      throw new ProductNotFoundException(orderDetail.ProductId);
+                        product.Status = Contract.Enum.Product.Status.Selling.Name;
+                    }
                 }
+                await _orderRepository.UpdateOrder(order);
+
+                transaction.Commit();
+                return true;
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                return false;
+            }
         }
 
         public async Task<Order> CreateOrderForTrade(CreateOrderRequest createOrderRequest)
@@ -190,6 +208,7 @@ namespace ExchangeGood.Service.UseCase
                 });
             }
 
+            await SendOrderTradeRequestEmail(createOrderRequest.MemberId);
             return await _orderRepository.AddOrder(order);
         }
 
@@ -207,5 +226,86 @@ namespace ExchangeGood.Service.UseCase
         {
             throw new NotImplementedException();
         }
+
+
+        private async Task SendOrderExchangeRequestEmail(string id)
+        {
+            var member = await _memberService.GetMemberByFeId(id);
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("ExchangeGood System", _smtpSetting.Username));
+            message.To.Add(new MailboxAddress("", member.Email));
+            message.Subject = "Order Exchange";
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = $@"
+            <p>Dear {member.UserName},</p>
+            <p>You have create a new order exchange request</p>
+            <p>Best regards,</p>
+            <p>The ExchangeGood Team </p>
+        ";
+
+            message.Body = bodyBuilder.ToMessageBody();
+
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_smtpSetting.SmtpServer, _smtpSetting.Port, _smtpSetting.UseSsl);
+                    await client.AuthenticateAsync(_smtpSetting.Username, _smtpSetting.Password);
+                    await client.SendAsync(message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to send email: {ex.Message}");
+                    throw;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                }
+            }
+        }
+
+
+        private async Task SendOrderTradeRequestEmail(string id)
+        {
+            var member = await _memberService.GetMemberByFeId(id);
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("ExchangeGood System", _smtpSetting.Username));
+            message.To.Add(new MailboxAddress("", member.Email));
+            message.Subject = "Order Trade";
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = $@"
+            <p>Dear {member.UserName},</p>
+            <p>You have create a new order trade request</p>
+            <p>Best regards,</p>
+            <p>The ExchangeGood Team </p>
+        ";
+
+            message.Body = bodyBuilder.ToMessageBody();
+
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_smtpSetting.SmtpServer, _smtpSetting.Port, _smtpSetting.UseSsl);
+                    await client.AuthenticateAsync(_smtpSetting.Username, _smtpSetting.Password);
+                    await client.SendAsync(message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to send email: {ex.Message}");
+                    throw;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                }
+            }
+        }
     }
+
+
+
 }
